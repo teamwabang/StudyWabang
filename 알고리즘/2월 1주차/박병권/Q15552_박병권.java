5bc42a26dfb0dc3fe959ea4e292ee50d91d01ffb
martin1341 <98645221+martin1341@users.noreply.github.com>
2022-02-02 23:16:26 +0900

    Algorithm

---
+++ 알고리즘/2월 1주차/박병권/Q15552_박병권.java
@@ -0,0 +1,16 @@
+package altudy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class T_15552 {
+
+	public static void main(String[] args) throws Exception, IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		String s = bf.readLine();
+		int i = Integer.parseInt(bf.readLine());
+
+	}
+
+}
