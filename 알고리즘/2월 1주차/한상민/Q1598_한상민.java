707ed532bd8173a90952cc234526872726e5ec63
tkdalsgks <hsm_01@naver.com>
2022-02-04 17:47:53 +0900

    Algorithm

--- 알고리즘/2월 1주차/한상민/Q1598_한상민.java
+++ 알고리즘/2월 1주차/한상민/Q1598_한상민.java
@@ -11,8 +11,8 @@ public class Q1598_한상민 {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
+		int a = Integer.parseInt(st.nextToken()) - 1;
+		int b = Integer.parseInt(st.nextToken()) - 1;
 		
 		int w = Math.abs((a / 4) - (b / 4));
 		int h = Math.abs((a % 4) - (b % 4));
