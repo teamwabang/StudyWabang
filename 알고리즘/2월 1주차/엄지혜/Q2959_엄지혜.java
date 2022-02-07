d86913960e03da61f23f7072a93d2e102e881898
jihye-12 <81624537+jihye-12@users.noreply.github.com>
2022-02-07 17:40:10 +0900

    Algorithm

---
+++ 알고리즘/2월 1주차/엄지혜/Q2959_엄지혜.java
@@ -0,0 +1,16 @@
+package test;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B2959 {
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+        int [] arr = new int[4];
+        for(int i=0;i<4;i++) {
+            arr[i]=sc.nextInt();
+        }
+        Arrays.sort(arr);
+        System.out.println(arr[0] * arr[2]);
+    }
+}
