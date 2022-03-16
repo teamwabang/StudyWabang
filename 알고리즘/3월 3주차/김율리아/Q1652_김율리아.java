cc27e651e03659ccd73c9eeb1b0719c22bb7c0db
yuuulya <yla101@naver.com>
2022-03-16 14:26:53 +0900

    3월 3주차 알고리즘 업로드 -율리아

---
+++ 알고리즘/3월 3주차/김율리아/Q1652_김율리아.java
@@ -0,0 +1,56 @@
+import java.util.*;
+
+public class Q1652_김율리아 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		String[][] arr = new String[N][N];
+	
+		int cnt = 0; 
+		int cnt2 = 0; 
+		int row = 0; 
+		int column = 0;
+		
+		//입력받은 것을 2차원 배열에 삽입
+		for(int i=0; i<N; i++) {
+			String[] line = sc.next().split("");
+			for(int j=0; j<N; j++) {
+	             arr[i][j] = line[j];
+			}
+		}
+				
+		for(int i=0; i<N; i++) {
+			for(int j=0; j<N; j++) {
+				//##가로
+				//빈 칸이 있을때 cnt++
+				if(arr[i][j].equals(".")) {
+					cnt++;
+				}
+				//짐이 있거나 마지막 값까지 조회했을때 cnt값 초기화 
+				//이때 cnt값이 2이상이라면 row++
+				if(arr[i][j].equals("X")||(j==N-1)) {
+					if(cnt>=2) { 
+						row++;
+					}
+					cnt=0;
+				}
+				//##세로
+				//j,i값 변경해서 세로 자리 조회
+				if(arr[j][i].equals(".")) {
+					cnt2++;
+				}
+				//짐이 있거나 마지막 값까지 조회했을때 cnt값 초기화 
+				//이때 cnt값이 2이상이라면 column++
+				if(arr[j][i].equals("X")||(j==N-1)) {
+					if(cnt2>=2) column++;
+					cnt2=0;
+				}
+			}		
+		}
+		System.out.println(row + " " +column);			
+	}//main
+}//class	
+
