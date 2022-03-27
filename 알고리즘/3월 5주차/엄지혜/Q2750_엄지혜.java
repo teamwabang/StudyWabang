38e6c6b9be7a7ffdff686f0893f377df36ce05b4
jihye-12 <81624537+jihye-12@users.noreply.github.com>
2022-03-27 22:45:16 +0900

    3월 5주차 알고리즘 업로드 - 지혜

---
+++ 알고리즘/3월 5주차/엄지혜/Q2750_엄지혜.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B2750 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+ 
+		Arrays.sort(arr);
+		
+		for(int a : arr) {
+			System.out.println(a);
+		}
+ 
+	}
+}
+
